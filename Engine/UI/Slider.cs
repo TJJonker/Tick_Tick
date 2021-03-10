ec4e10210d1e605641f74230b359eadc1b09a389
CptDads <tjj.jonker@gmail.com>
2021-03-10 21:56:18 +0100

    Updated the switch tab of the engine

--- Engine/UI/Slider.cs
+++ Engine/UI/Slider.cs
@@ -4,17 +4,42 @@ namespace Engine.UI
 {
     public class Slider : GameObjectList
     {
+        // Sprites for the fore and background of the slider
         private SpriteGameObject back, front;
+
+        // The minimum and maximum value of the slider
         private float minValue, maxValue;
+
+        // The current value and the value in the previous frame
         private float currentValue, previousValue;
+
+        // The number of pixels the foreground stays away from the border
         private float padding;
 
+        // The difference between the minimum and maximum value that the slider can store
         private float Range { get { return maxValue - minValue; } }
-        private float minimumLocalX { get { return padding + front.Width / 2; } }
-        private float maximumLocalX { get { return back.Width - padding - front.Width / 2; } }
-        private float availableWidth { get { return maximumLocalX - minimumLocalX; } }
-        public bool ValueChanged { get { return currentValue != previousValue; } }
 
+        // The smallest X coordinate the front image may have
+        private float minimumLocalX { get { return padding + front.Width / 2; } }
+
+        // The largest X coordinatie the front image may have
+        private float maximumLocalX { get { return back.Width - padding - front.Width / 2; } }
+
+        // The total width that is available for the front image
+        private float availableWidth { get { return maximumLocalX - minimumLocalX; } }
+
+        /// <summary>
+        /// Returns whether the slider's value has changed last frame
+        /// </summary>
+        public bool ValueChanged
+        {
+            get { return currentValue != previousValue; }
+        }
+
+        /// <summary>
+        /// Gets or sets the current numeric value that's stored in this slider
+        /// When the value is changed, the foreground image will move to the correct position
+        /// </summary>
         public float Value
         {
             get { return currentValue; }
@@ -29,17 +54,21 @@ namespace Engine.UI
 
         public Slider(string backGroundsprite, string foreGroundSprite, float minValue, float maxValue, float padding)
         {
-            back = new SpriteGameObject(backGroundsprite);
+            // Add the background sprite
+            back = new SpriteGameObject(backGroundsprite, 0.9f);
             AddChild(back);
 
-            front = new SpriteGameObject(foreGroundSprite);
+            // Add the foreground sprite
+            front = new SpriteGameObject(foreGroundSprite, 0.95f);
             front.Origin = new Vector2(front.Width / 2, 0);
             AddChild(front);
 
+            // Store the other values
             this.minValue = minValue;
             this.maxValue = maxValue;
             this.padding = padding;
 
+            // By default, start with the minimum value
             previousValue = this.minValue;
             Value = previousValue;
         }
@@ -49,13 +78,16 @@ namespace Engine.UI
             base.HandleInput(inputHelper);
             if (!Visible) return;
 
+            // Stores the previous slider value
             previousValue = Value;
 
             Vector2 mousePos = inputHelper.MousePositionWorld;
             if (inputHelper.MouseLeftButtonDown() && back.BoundingBox.Contains(mousePos))
             {
+                // Translate the mouse position to a number between 0 and 1
                 float correctedX = mousePos.X - GlobalPosition.X - minimumLocalX;
                 float newFraction = correctedX / availableWidth;
+                // Converts that to a new slider value
                 Value = newFraction * Range + minValue;
             }
         }
--- Engine/UI/Switch.cs
+++ Engine/UI/Switch.cs
@@ -1,8 +1,16 @@
 ﻿namespace Engine.UI
 {
+    /// <summary>
+    /// A class that van represent a UI switch that can be turner on or off
+    /// </summary>
     public class Switch : Button
     {
         private bool selected;
+
+        /// <summary>
+        /// Whether or not this switch is turned on or off
+        /// When toggled, the switch will receive a new sprite
+        /// </summary>
         public bool Selected
         {
             get { return selected; }
@@ -16,7 +24,12 @@
             }
         }
 
-        public Switch(string assetName) : base(assetName)
+        /// <summary>
+        /// Creates a new <see cref="Switch"/> with the given sprite and depth
+        /// </summary>
+        /// <param name="assetName">The name of the sprite to use</param>
+        /// <param name="depth">The depth aty which the switch should be drawn</param>
+        public Switch(string assetName, float depth) : base(assetName, depth)
         {
             Selected = false;
         }
